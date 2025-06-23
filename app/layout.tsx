61d58bef78f79a77a6c1696bc95ba1b2409df8cc
Karenffff <karenfurst6@gmail.com>
2025-06-23 18:14:57 +0100

    viewpoint issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ const ibmPlexSerif = IBM_Plex_Serif({
 
 export const metadata: Metadata = {
   title: "Bethel credit union",
+  
   description: "Bethel credit union is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
